04087d0f43c0d6e49b4183bfbad8b6fa2a712fb4
Asifa-Erum <asifaerum2506@gmail.com>
2025-01-28 09:49:57 +0500

    Add files via upload

---
+++ asifa js 25r (3)/Es6.js
@@ -0,0 +1,14 @@
+// function main(a,b = main(a,b)){
+//     return a+b;
+// }
+// console.log(main(3,7));
+
+
+
+// function hello(){
+//     return "Hi";
+// }
+// function greet(a,b=hello()){
+//     return a + " " + b;
+// }
+// console.log(greet(hello(),"Owais"));
---
+++ asifa js 25r (3)/index.js
@@ -0,0 +1,3 @@
+// let user = "Owais";
+    // let checkNum = user ; "Right" ? user  != "Ali"  : "Wrong"
+    // console.log(checkNum);
